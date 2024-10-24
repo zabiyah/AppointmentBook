8541b224cc46a79bbbb60a18f10ad5d516f69a6d
zabiyah <zabiyahk@nycstudents.net>
2024-10-24 15:49:37 +0000

    mr holmer pls give us google doc tutorials

---
+++ AssignmentBook.java
@@ -0,0 +1,8 @@
+public class AssignmentBook{
+    private boolean isMinuteFree(int period, int minute){
+        return((1<= period <=8)&&(0<=startMinute<=59));
+    }
+    private void reserveBlock(int period,int startMinute, int duration){
+        
+    }
+}
